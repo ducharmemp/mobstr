5227eac51502b3480bdecbdb66f4078d53df6c60
Matthew DuCharme <ducharmemp@gmail.com>
2019-08-31 01:14:31 -0400

    Reverted package dependencies until I can get around to updating

--- test/perf.test.ts
+++ test/perf.test.ts
@@ -3,6 +3,7 @@ import { expect } from 'chai';
 import { range, countBy, entries } from "lodash";
 import { v4 as uuid } from "uuid";
 
+import logger from '../src/logger';
 import { initializeStore, CascadeOptions, IntegrityError, TriggerExecutionStrategy } from "../dist/index.js";
 
 console.log(TriggerExecutionStrategy.Intercept)
@@ -67,24 +68,34 @@ describe("#performance", (): void => {
   })
 
   it("should be performant in adding a 500 item subset of the items to a collection", (): void => {
+    logger.profile('500');
     addAll(subset);
+    logger.profile('500');
   });
 
   it("should be performant in adding 100k amounts of items to a collection", (): void => {
+    logger.profile('100k');
     addAll(foos);
+    logger.profile('100k');
   });
 
   it("should be performant in adding 100k items and then dropping a collection", (): void => {
+    logger.profile('addAllAndTruncate');
     addAll(foos);
     truncateCollection(Foo);
+    logger.profile('addAllAndTruncate');
   });
 
   it("should be performant in adding 100k items with a constraint", (): void => {
+    logger.profile('addAllConstraint');
     check(Foo, "number", value => value > 0);
     addAll(foos);
+    logger.profile('addAllConstraint');
   });
 
   it("should be performant in adding 100k items with multiple decorator constraints", (): void => {
+    logger.profile('allAllDecorator');
     addAll(checkedFoos);
+    logger.profile('allAllDecorator');
   });
 });
